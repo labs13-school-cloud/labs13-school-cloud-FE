9ef71b93d6d76d7c15938e6af40aa11f8be1236c
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-03-20 18:03:33 -0400

    Set up basic routes, add button to link to TeamMembers view

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { LandingPageView } from './components/LandingPage';
+import { TeamMembersView } from './components/TeamMembers';
 
 class App extends Component {
   render() {
     return (
       <>
-        
+        <CssBaseline />
+        <Route exact path="/" component={LandingPageView} />
+        {/* Note from Leigh-Ann: This component will need to be restructured with proper routes, displaying training-series is a placeholder */}
+        <Route path="/training-series" component={TeamMembersView} />
       </>
     );
   }
--- client/src/components/TeamMembers/TeamMembersView.js
+++ client/src/components/TeamMembers/TeamMembersView.js
@@ -1,1 +1,12 @@
 // component to contain all the components related to team members
+import React from 'react';
+
+const TeamMembersView = props => {
+    return (
+        <>
+            <p>Team Members View</p>
+        </>
+    )
+}
+
+export default TeamMembersView;
