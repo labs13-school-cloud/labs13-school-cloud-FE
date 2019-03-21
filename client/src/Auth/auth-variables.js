01381e265531a3fe47d597dd0c88149ee6ba09f0
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-03-20 20:36:16 -0400

    Navigation tabs UI for dashboard, changed auth route

--- client/src/Auth/auth-variables.js
+++ client/src/Auth/auth-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: "blent.auth0.com",
   clientId: "PCAD70wIHa7FLtbGoeqmOCPKlaZ18DpN",
-  callbackUrl: "http://localhost:3000/training-series"
+  callbackUrl: "http://localhost:3000/home"
 };
---
+++ client/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+// parent component for app once logged in
+import React from 'react';
+import { NavigationView } from '../Navigation';
+
+const Dashboard = props => {
+    return (
+        <>
+            <NavigationView />
+        </>
+    )
+}
+
+export default Dashboard;
