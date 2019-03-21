5da25ddf5720421a41f27e04ec729aaa64beb5e0
Mike Landers <landers.mike@gmail.com>
2019-03-21 16:20:37 -0700

    Added logout button to profile page

---
+++ client/src/components/Profile/profile.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+
+import Button from '@material-ui/core/Button';
+
+function Profile(props) {
+	const [uProfile, setUProfile] = useState({});
+
+	const handleLogout = e => {
+		e.preventDefault();
+		localStorage.removeItem('isLoggedIn');
+		props.history.push('/');
+	};
+
+	useEffect(() => {
+		const { userProfile, getProfile } = props.auth;
+
+		if (!userProfile) {
+			getProfile((err, profile) => {
+				setUProfile(profile);
+			});
+		} else {
+			setUProfile(userProfile);
+		}
+	}, []);
+	return (
+		<div className="container">
+			<Button variant="contained" color="secondary" onClick={e => handleLogout(e)}>
+				Log Out
+			</Button>
+			<div className="profile-area">
+				<h1>{uProfile.name}</h1>
+				<div>
+					<img src={uProfile.picture} alt="profile" />
+					<div>
+						<h3>{uProfile.nickname}</h3>
+					</div>
+					<pre>{JSON.stringify(uProfile, null, 2)}</pre>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Profile;
