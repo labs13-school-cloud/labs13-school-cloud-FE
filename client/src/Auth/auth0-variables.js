e5eabf91eca6fac5d8332eed7f94bd827d7408ec
Mike Landers <landers.mike@gmail.com>
2019-03-21 16:51:38 -0700

    added commented callback urls

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -3,4 +3,5 @@ export const AUTH_CONFIG = {
 	clientId: 'Wx4vlkjhmyHGGfjqWXUYUKvyfRB0E9G5',
 	callbackUrl: process.env.REACT_APP_LOCAL || process.env.REAC_APP_PROD,
 	// callbackUrl: 'http://localhost:3000/callback',
+	// callbackUrl: 'https://trainingbot-dev.netlify.com/callback',
 };
