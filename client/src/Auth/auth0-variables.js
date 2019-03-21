04fe61eaac16e5186626f6a37ebde17c47429c49
Brandon Lent <blent1050@gmail.com>
2019-03-21 18:17:32 -0400

    callbackUrl hotfix

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: "trainingbot.auth0.com",
   clientId: "Wx4vlkjhmyHGGfjqWXUYUKvyfRB0E9G5",
-  callbackUrl: "http://localhost:3000/callback"
+  callbackUrl: "https://trainingbot-dev.netlify.com/callback"
 };
