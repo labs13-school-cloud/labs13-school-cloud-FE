989d4014794d92fe4dc04ed366bc9814dde3e2d4
Brandon Lent <blent1050@gmail.com>
2019-03-20 21:39:02 -0400

    Implemented new auth information pointing to our new Auth0 acct

--- client/src/Auth/auth-variables.js
+++ client/src/Auth/auth-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
-  domain: "blent.auth0.com",
-  clientId: "PCAD70wIHa7FLtbGoeqmOCPKlaZ18DpN",
+  domain: "trainingbot.auth0.com",
+  clientId: "Wx4vlkjhmyHGGfjqWXUYUKvyfRB0E9G5",
   callbackUrl: "http://localhost:3000/home"
 };
