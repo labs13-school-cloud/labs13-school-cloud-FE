398d67c81c068d272bbf6165d648d929220180f0
Mike Landers <landers.mike@gmail.com>
2019-03-21 16:25:39 -0700

    switched auth callbacks

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
 	domain: 'trainingbot.auth0.com',
 	clientId: 'Wx4vlkjhmyHGGfjqWXUYUKvyfRB0E9G5',
-	// callbackUrl: "https://trainingbot-dev.netlify.com/callback"
-	callbackUrl: 'http://localhost:3000/callback',
+	callbackUrl: 'https://trainingbot-dev.netlify.com/callback',
+	// callbackUrl: 'http://localhost:3000/callback',
 };
