de8e3441a58c2431a1eeefe59c2a3f8aadcf0684
Mike Landers <landers.mike@gmail.com>
2019-03-22 08:19:03 -0700

    fixing typo for auth variables

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,8 +1,7 @@
 export const AUTH_CONFIG = {
-
 	domain: 'trainingbot.auth0.com',
 	clientId: 'Wx4vlkjhmyHGGfjqWXUYUKvyfRB0E9G5',
-	callbackUrl: process.env.REACT_APP_LOCAL || process.env.REAC_APP_PROD,
+	callbackUrl: process.env.REACT_APP_LOCAL || process.env.REACT_APP_PROD,
 	// callbackUrl: 'http://localhost:3000/callback',
 	// callbackUrl: 'https://trainingbot-dev.netlify.com/callback',
 };
