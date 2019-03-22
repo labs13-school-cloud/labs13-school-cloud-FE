df3a1a9d395bbbe93e6e5645abf1f577f1fef72c
Mike Landers <landers.mike@gmail.com>
2019-03-22 16:51:45 -0700

    Added app bar and menu items

---
+++ client/src/components/AppBar/AppBar.js
@@ -0,0 +1,66 @@
+// navigation includes tab navigation, breadcrumbs, user avatar
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+// import Typography from '@material-ui/core/Typography';
+// import Tabs from '@material-ui/core/Tabs';
+// import Tab from '@material-ui/core/Tab';
+import Button from '@material-ui/core/Button';
+
+import AppBar from '@material-ui/core/AppBar';
+import logo from '../../img/training-bot.png';
+import { flex } from '@material-ui/system/flexbox';
+
+const styles = {
+	avatar: {
+		margin: 10,
+	},
+
+	appBar: {
+		display: 'flex',
+		justifyContent: 'space-between',
+		flexDirection: 'row',
+		color: 'white',
+		backgroundColor: '',
+		padding: 5,
+	},
+	logo: {
+		width: 50,
+		height: 50,
+		marginRight: 10,
+	},
+	menuItems: {
+		display: 'flex',
+		alignItems: 'center',
+		color: 'white',
+	},
+};
+
+const appBar = props => {
+	const { classes } = props;
+	const [value, setValue] = React.useState(2);
+
+	function handleChange(event, newValue) {
+		setValue(newValue);
+	}
+
+	return (
+		<>
+			<AppBar className={classes.appBar}>
+				<div className={classes.menuItems}>
+					<img src={logo} alt="logo" className={classes.logo} />
+					<Button>About</Button>
+					<Button>Contact</Button>
+				</div>
+				<Avatar alt="Avatar" src="/static/images/avatar/1.jpg" className={classes.avatar} />
+			</AppBar>
+		</>
+	);
+};
+
+appBar.propTypes = {
+	classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(appBar);
