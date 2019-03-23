cb68ca6f2af94245e5f5f9ccf6aab52a420a9925
Nate Boyette <nateboyette@icloud.com>
2019-03-22 20:00:10 -0700

    Added temp styles so that app bar isn't covering the tabs.

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import TeamMembersView from "../TeamMembers/TeamMembersView";
 import TrainingSeriesView from "../TrainingSeries/TrainingSeriesView";
 import AppBar from "../AppBar/AppBar";
-
+import styled from "styled-components";
 class Dashboard extends React.Component {
   state = {
     tabValue: 0
@@ -27,34 +27,41 @@ class Dashboard extends React.Component {
 
     return (
       <>
-        <NavigationView
-          tabValue={this.state.tabValue}
-          changeTabValue={this.changeTabValue}
-        />
-        {isAuthenticated() && (
-          <>
+        <AppBar />
+        <DashboardContainer>
+          <NavigationView
+            tabValue={this.state.tabValue}
+            changeTabValue={this.changeTabValue}
+          />
+          {isAuthenticated() && (
+            <>
+              <h4>
+                You are logged in! You can now view your{" "}
+                <Link to="profile">profile area</Link>.
+              </h4>
+              <div>
+                {this.state.tabValue === 0 && <TrainingSeriesView />}
+                {this.setState.tabValue === 1 && <TeamMembersView />}
+              </div>
+            </>
+          )}
+          {!isAuthenticated() && (
             <h4>
-              You are logged in! You can now view your{" "}
-              <Link to="profile">profile area</Link>.
+              You are not logged in! Please{" "}
+              <a style={{ cursor: "pointer" }} onClick={this.login.bind(this)}>
+                Log In
+              </a>{" "}
+              to continue.
             </h4>
-            <div>
-              {this.state.tabValue === 0 && <TrainingSeriesView />}
-              {this.setState.tabValue === 1 && <TeamMembersView />}
-            </div>
-          </>
-        )}
-        {!isAuthenticated() && (
-          <h4>
-            You are not logged in! Please{" "}
-            <a style={{ cursor: "pointer" }} onClick={this.login.bind(this)}>
-              Log In
-            </a>{" "}
-            to continue.
-          </h4>
-        )}
+          )}
+        </DashboardContainer>
       </>
     );
   }
 }
+
+const DashboardContainer = styled.div`
+  margin: 100px 0;
+`;
 
 export default Dashboard;
