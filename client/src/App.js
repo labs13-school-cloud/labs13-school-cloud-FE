8dc6eeb2315205ec29ece2ea935236f996736151
Brandon Lent <blent1050@gmail.com>
2019-03-25 11:24:46 -0400

    Changed how Auth works. Everything is now a function export instead of just one big class

--- client/src/App.js
+++ client/src/App.js
@@ -1,75 +1,24 @@
-import React, { Component } from 'react';
-// Routing
-import { Route } from 'react-router-dom';
+import React, { Component } from "react";
 
 //Styling
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Button from '@material-ui/core/Button';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Button from "@material-ui/core/Button";
 
 //Components
-import { LandingPageView } from './components/LandingPage';
-import { TeamMembersView } from './components/TeamMembers';
-import { Dashboard } from './components/Dashboard';
+import { LandingPageView } from "./components/LandingPage";
+import { TeamMembersView } from "./components/TeamMembers";
+import { Dashboard } from "./components/Dashboard";
 
+import { login, logout, isLoggedIn } from "./Auth/Auth";
 
 class App extends Component {
-  goTo(route) {
-    this.props.history.replace(`/${route}`);
-  }
-
-  login() {
-    this.props.auth.login();
-  }
-
-  logout() {
-    this.props.auth.logout();
-  }
-
-  renewToken() {
-    const { renewSession } = this.props.auth;
-    renewSession();
-  }
-
-  componentDidMount() {
-    const { renewSession } = this.props.auth;
-
-    if (localStorage.getItem('isLoggedIn') === 'true') {
-      renewSession();
-    }
-  }
-
   render() {
-    console.log(this.props.auth);
-    const { isAuthenticated } = this.props.auth;
-
     return (
       <>
         <CssBaseline />
-        <Button
-          onClick={() => this.props.auth.login()}
-          color="primary"
-          variant="contained"
-        >
+        <Button onClick={() => login()} color='primary' variant='contained'>
           Register
         </Button>
-        {!isAuthenticated() && (
-          <Button
-            bsStyle="primary"
-            className="btn-margin"
-            onClick={this.login.bind(this)}
-          >
-            Log In
-          </Button>
-        )}
-        {isAuthenticated() && (
-          <Button
-            bsStyle="primary"
-            className="btn-margin"
-            onClick={this.logout.bind(this)}
-          >
-            Log Out
-          </Button>
-        )}
       </>
     );
   }
--- client/src/Callback/callback.js
+++ client/src/Callback/callback.js
@@ -1,27 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 //Loading SVG that gets displayed
-import loading from './loading.svg';
+import loading from "./loading.svg";
+
+import { setIdToken, setAccessToken } from "../Auth/Auth";
 
 class Callback extends Component {
+  componentDidMount() {
+    setAccessToken();
+    setIdToken();
+    window.location.href = "/home";
+  }
   render() {
     //Customized styling
     const style = {
-      position: 'absolute',
-      display: 'flex',
-      justifyContent: 'center',
-      height: '100vh',
-      width: '100vw',
+      position: "absolute",
+      display: "flex",
+      justifyContent: "center",
+      height: "100vh",
+      width: "100vw",
       top: 0,
       bottom: 0,
       left: 0,
       right: 0,
-      backgroundColor: 'white',
+      backgroundColor: "white"
     };
 
     return (
       <div style={style}>
-        <img src={loading} alt="loading" />
+        <img src={loading} alt='loading' />
       </div>
     );
   }
