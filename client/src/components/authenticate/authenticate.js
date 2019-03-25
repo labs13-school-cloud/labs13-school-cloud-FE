0ba7879647fb3870d3fa955a099728674060b831
Brandon Lent <blent1050@gmail.com>
2019-03-25 19:48:02 -0400

    Added styling to HOC

--- client/src/components/authenticate/authenticate.js
+++ client/src/components/authenticate/authenticate.js
@@ -2,6 +2,11 @@
 
 import React, { Component } from "react";
 
+//Styling
+import styled from "styled-components";
+import Logo from "../../img/training-bot.png";
+
+//Authentication
 import { login } from "../../Auth/Auth";
 
 export default function(Component) {
@@ -10,12 +15,39 @@ export default function(Component) {
       const token = localStorage.getItem("id_token");
 
       const notLoggedIn = (
-        <div>
-          Please Login to see the users
-          <button onClick={e => login()}>Login</button>
-        </div>
+        <LoginContainer>
+          <LoginContent>
+            <LogoContainer src={Logo} />
+            <p>Please Login to see the users</p>
+            <button onClick={e => login()}>Login</button>
+          </LoginContent>
+        </LoginContainer>
       );
       return <>{token ? <Component {...this.props} /> : notLoggedIn}</>;
     }
   };
 }
+
+const LoginContainer = styled.div`
+  min-height: 100vh;
+  min-width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  p {
+    color: white;
+  }
+`;
+const LoginContent = styled.div`
+  height: 250px;
+  width: 25vw;
+  display: flex;
+  flex-direction: column;
+  background-color: #2699fb;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LogoContainer = styled.img`
+  width: 100px;
+`;
