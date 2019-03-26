76e3e8a51fd8b8284d58b6fa1ed99dcd8b9a0d54
Brandon Lent <blent1050@gmail.com>
2019-03-26 16:15:52 -0400

    Implemented button that opens modal to add new training series

--- client/src/components/TrainingSeries/TrainingSeries.js
+++ client/src/components/TrainingSeries/TrainingSeries.js
@@ -11,7 +11,6 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import { spacing } from "@material-ui/system";
 
 //Customized Styling
 const styles = {
