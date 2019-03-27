8fcd9d10ee0e4bcc92768756609ee445e768b48b
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-03-27 18:29:59 -0400

    Boilerplate postsActions and postsReducer for GET posts, GET post/id, POST post

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,11 +1,13 @@
 import teamMemberReducer from "./teamMembersReducer";
 import trainingSeriesReducer from "./trainingSeriesReducer";
+import postsReducer from './postsReducer';
 
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
   teamMemberReducer,
-  trainingSeriesReducer
+  trainingSeriesReducer,
+  postsReducer
 });
 
 export default rootReducer;
