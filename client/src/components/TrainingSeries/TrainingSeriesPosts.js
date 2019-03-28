79f3ec0d977476bf6b90342a9025e8d05a46a18c
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-03-28 16:57:00 -0400

    Move methods and state into TrainingSeriesPosts to generate auto rerender with newly added post

--- client/src/components/TrainingSeries/TrainingSeriesPosts.js
+++ client/src/components/TrainingSeries/TrainingSeriesPosts.js
@@ -15,9 +15,15 @@ import PropTypes from "prop-types";
 import Button from "@material-ui/core/Button";
 
 class TrainingSeriesPosts extends React.Component {
-  state = {
-    
-  };
+    state = {
+        open: false,
+        post: {
+          postName: "",
+          postDetails: "",
+          link: "",
+          daysFromStart: 1, 
+        }
+      };
 
   componentDidMount() {
     this.getTrainingSeriesWithPosts(this.props.match.params.id);
@@ -26,6 +32,49 @@ class TrainingSeriesPosts extends React.Component {
   getTrainingSeriesWithPosts = id => {
     this.props.getTrainingSeriesPosts(id);
   };
+
+  handleOpen = () => {
+    this.setState({ 
+      open: true,
+      post: {
+        ...this.state.post,
+        trainingSeriesID: this.props.singleTrainingSeries.trainingSeriesID
+      }
+     });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  handleChange = e => {
+    e.preventDefault();
+    this.setState({
+      ...this.state,
+      post: {
+        ...this.state.post,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  addAPost = e => {
+    e.preventDefault();
+    console.log("post", this.state.post)
+    this.props.createAPost(this.state.post);
+    this.setState({
+      ...this.state,
+      open: false,
+      post: {
+        ...this.state.post,
+        postName: "",
+        postDetails: "",
+        link: "",
+        daysFromStart: 1,
+      }
+    }, this.getTrainingSeriesWithPosts(this.props.match.params.id))
+  };
+
   render() {
     // const series = this.props.trainingSeries.find(
     //   series => `${series.trainingSeriesID}` === this.props.match.params.id
@@ -36,6 +85,12 @@ class TrainingSeriesPosts extends React.Component {
         <PostModal
           trainingSeries={this.props.singleTrainingSeries}
           createAPost={this.props.createAPost}
+          addAPost={this.addAPost}
+          handleChange={this.handleChange}
+          handleOpen={this.handleOpen}
+          handleClose={this.handleClose}
+          open={this.state.open}
+          post={this.state.post}
         />
         {this.props.isLoading && <p>Please wait...</p>}
         {!this.props.isLoading && (
@@ -43,7 +98,7 @@ class TrainingSeriesPosts extends React.Component {
             <p>{this.props.singleTrainingSeries.title}</p>
             <div>
               {this.props.posts.map(post => (
-                <div>
+                <div key={post.postID}>
                   <p>{post.postName}</p>
                 </div>
               ))}
