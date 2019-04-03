eeb22e3002fc1210028ab4b5f52d84778cff5f6a
Brandon <Blent1050@yahoo.com>
2019-04-03 18:07:43 -0400

    Implemented drop down

--- client/src/components/Modals/addTeamMemberToTrainingSeriesModal.js
+++ client/src/components/Modals/addTeamMemberToTrainingSeriesModal.js
@@ -1,25 +1,27 @@
-import React from "react";
+import React from 'react';
 
-import DatePicker from "react-datepicker";
+import DatePicker from 'react-datepicker';
 
 //Styles
-import "react-datepicker/dist/react-datepicker.css";
-import { withStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-import Modal from "@material-ui/core/Modal";
-import Button from "@material-ui/core/Button";
-import AddIcon from "@material-ui/icons/Add";
-import Fab from "@material-ui/core/Fab";
-import Checkbox from "@material-ui/core/Checkbox";
-import FormLabel from "@material-ui/core/FormLabel";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import styled from "styled-components";
-import Radio from "@material-ui/core/Radio";
-import RadioGroup from "@material-ui/core/RadioGroup";
+import 'react-datepicker/dist/react-datepicker.css';
+import {withStyles} from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Modal from '@material-ui/core/Modal';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import styled from 'styled-components';
+import Select from '@material-ui/core/Select';
+import FormControl from '@material-ui/core/FormControl';
+import {MenuItem} from '@material-ui/core';
+
 //REDUX
-import { connect } from "react-redux";
-import { addTeamMemberToTrainingSeries } from "../../store/actions/";
-import { TransitionGroup } from "react-transition-group";
+import {connect} from 'react-redux';
+import {addTeamMemberToTrainingSeries} from '../../store/actions/';
+import {TransitionGroup} from 'react-transition-group';
 
 function getModalStyle() {
   const top = 50;
@@ -28,85 +30,85 @@ function getModalStyle() {
   return {
     top: `${top}%`,
     left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`
+    transform: `translate(-${top}%, -${left}%)`,
   };
 }
 
 const styles = theme => ({
   paper: {
-    position: "absolute",
+    position: 'absolute',
     width: 400,
     backgroundColor: theme.palette.background.paper,
     boxShadow: theme.shadows[5],
     padding: theme.spacing.unit * 4,
-    outline: "none"
+    outline: 'none',
   },
   container: {
-    display: "flex",
-    flexWrap: "wrap"
+    display: 'flex',
+    flexWrap: 'wrap',
   },
   textField: {
     marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit,
-    width: 300
+    width: 300,
   },
   dense: {
-    marginTop: 19
+    marginTop: 19,
   },
   menu: {
-    width: 200
+    width: 200,
   },
   button: {
-    margin: theme.spacing.unit
+    margin: theme.spacing.unit,
   },
   memberList: {
-    display: "flex",
-    flexDirection: "column"
-  }
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  selectEmpty: {
+    marginTop: theme.spacing.unit * 2,
+  },
 });
 
 class UserModal extends React.Component {
   state = {
     open: false,
-    trainingSeriesID: "",
-    startDate: "",
-    value: ""
+    trainingSeriesID: '',
+    startDate: '',
+    value: '',
   };
 
   componentDidMount() {
-    if (this.props.modalType === "assignMultiple") {
-      let d = new Date();
-      let formattedDate = d.toISOString();
-      this.setState({
-        trainingSeriesID: this.props.trainingSeriesID,
-        startDate: formattedDate
-      });
-    } else {
-      this.setState({ email: this.props.email, name: this.props.name });
-    }
+    let d = new Date();
+    let formattedDate = d.toISOString();
+    this.setState({
+      trainingSeriesID: this.props.trainingSeriesID,
+      startDate: formattedDate,
+    });
   }
   componentDidUpdate(prevProps) {
     if (prevProps.isEditing) {
-      this.setState({ email: this.props.email, name: this.props.name });
+      this.setState({email: this.props.email, name: this.props.name});
     }
   }
 
   handleOpen = () => {
-    this.setState({ open: true });
+    this.setState({open: true});
   };
 
   handleClose = () => {
-    this.setState({ open: false });
+    this.setState({open: false});
   };
 
   handleChange = event => {
-    this.setState({ value: event.target.value });
+    this.setState({[event.target.name]: event.target.value});
+    console.log(this.state);
   };
 
   handleDateChange = date => {
     let d = date;
     this.setState({
-      startDate: d.toISOString()
+      startDate: d.toISOString(),
     });
   };
 
@@ -115,52 +117,44 @@ class UserModal extends React.Component {
     const data = {
       startDate: this.state.startDate,
       trainingSeriesID: this.state.selectedTrainingSeries,
-      assignments: [this.props.urlId]
+      assignments: [this.props.urlId],
     };
     this.props.addTeamMemberToTrainingSeries(data);
     this.handleClose();
     console.log(data);
   };
 
-  render() {
-    const { classes } = this.props;
-
-    let modalMap;
-    let modalTitle;
-    if (this.props.modalType === "assignMultiple") {
-      modalMap = this.props.trainingSeries.map(series => (
-        <>
-          <FormControlLabel
-            control={<Radio />}
-            label={`${series.title}`}
-            value={series.trainingSeriesID}
-          />
-        </>
-      ));
-      modalTitle = (
-        <FormLabel component='legend'>Assign Training Series</FormLabel>
+  renderTrainingSeriesInDropDown = () => {
+    return this.props.trainingSeries.map(series => {
+      console.log(series);
+      return (
+        <MenuItem
+          name="trainingSeriesID"
+          label={`${series.title}`}
+          value={series.trainingSeriesID}
+        >
+          {series.title}
+        </MenuItem>
       );
-    } else {
-      modalMap = this.props.trainingSeries.map(t => <p>{t.title}</p>);
-      modalTitle = "Assign Training Series";
-    }
+    });
+  };
 
-    console.log("MODAL", this.state.trainingSeriesID);
-
+  render() {
+    const {classes} = this.props;
     return (
       <>
-        <Fab color='primary' aria-label='Add' className={classes.fab}>
+        <Fab color="primary" aria-label="Add" className={classes.fab}>
           <AddIcon onClick={this.handleOpen} />
         </Fab>
 
         <Modal
-          aria-labelledby='simple-modal-title'
-          aria-describedby='simple-modal-description'
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
           open={this.state.open}
           onClose={this.handleClose}
         >
           <div style={getModalStyle()} className={classes.paper}>
-            <Typography variant='h6' id='modal-title'>
+            <Typography variant="h6" id="modal-title">
               Assign Training Series
             </Typography>
             <DatePicker
@@ -169,19 +163,21 @@ class UserModal extends React.Component {
               onChange={this.handleDateChange}
             />
             <form
-              variant='body1'
-              id='modal-title'
+              variant="body1"
+              id="modal-title"
               className={classes.memberList}
               onSubmit={e => this.handleSubmit(e)}
             >
-              <RadioGroup
-                onChange={this.handleChange}
-                name='TrainingSeries'
-                value={this.state.value}
-              >
-                {modalMap}
-              </RadioGroup>
-              <Button type='submit'>Submit</Button>
+              <FormControl className={''}>
+                <Select
+                  value={this.state.trainingSeriesID}
+                  onChange={this.handleChange}
+                  name="trainingSeriesID"
+                >
+                  {this.renderTrainingSeriesInDropDown()}
+                </Select>
+              </FormControl>
+              <Button type="submit">Submit</Button>
             </form>
           </div>
         </Modal>
@@ -194,7 +190,7 @@ const mapStateToProps = state => {
   return {
     trainingSeries: state.trainingSeriesReducer.trainingSeries,
     isLoading: state.userReducer.isLoading,
-    teamMembers: state.teamMembersReducer.teamMembers
+    teamMembers: state.teamMembersReducer.teamMembers,
   };
 };
 
@@ -203,6 +199,6 @@ const UserModalWrapped = withStyles(styles)(UserModal);
 export default connect(
   mapStateToProps,
   {
-    addTeamMemberToTrainingSeries
+    addTeamMemberToTrainingSeries,
   }
 )(UserModalWrapped);
