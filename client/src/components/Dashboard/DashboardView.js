5d63c74dd23b337218b91745ee3956ac82441b57
Brandon <Blent1050@yahoo.com>
2019-04-10 13:06:42 -0400

    restructered addteammember to require fields, move buttons to bottom and autofocus to first name on mount

--- client/src/components/Dashboard/DashboardView.js
+++ client/src/components/Dashboard/DashboardView.js
@@ -1,6 +1,6 @@
 // parent component for app once logged in
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import {Router, Route} from 'react-router-dom';
 
 import history from '../../history';
 
@@ -19,12 +19,12 @@ import CreateTrainingSeries from '../TrainingSeries/CreateTrainingSeries';
 import ReturnToDashboardButton from '../Navigation/ReturnToDashboard';
 
 //Auth
-import { getUserProfile } from '../../Auth/Auth';
+import {getUserProfile} from '../../Auth/Auth';
 import Authenticate from '../authenticate/authenticate';
 
 //State Management
-import { connect } from 'react-redux';
-import { getUser } from '../../store/actions/userActions';
+import {connect} from 'react-redux';
+import {getUser} from '../../store/actions/userActions';
 import TrainingSeriesPosts from '../TrainingSeries/TrainingSeriesPosts';
 import AddTeamMemberPage from '../TeamMembers/TeamMemberPageContainer/AddTeamMemberPage';
 import CreatePost from '../TrainingSeries/CreatePost';
@@ -32,149 +32,151 @@ import PostPage from '../TrainingSeries/PostPage';
 import NotificationsView from '../Notifications/NotificationsView';
 
 class Dashboard extends React.Component {
-	state = {
-		tabValue: 0,
-	};
+  state = {
+    tabValue: 0,
+  };
 
-	componentDidMount() {
-		getUserProfile(() => {
-			this.props.getUser();
-		});
-	}
+  componentDidMount() {
+    getUserProfile(() => {
+      this.props.getUser();
+    });
+  }
 
-	renderDashboard = () => {
-		const { user } = this.props.userProfile;
-		return (
-			<>
-				<TripleColumn>
-					<SmallColumns>
-						<TeamMembersView userId={user.userID} />
-						<TrainingSeriesView userId={user.userID} match={this.props.match} />
-					</SmallColumns>
-					<NotificationsView userId={user.userID}/>
-				</TripleColumn>
-			</>
-		);
-	};
+  renderDashboard = () => {
+    const {user} = this.props.userProfile;
+    return (
+      <>
+        <TripleColumn>
+          <SmallColumns>
+            <TeamMembersView userId={user.userID} />
+            <TrainingSeriesView userId={user.userID} match={this.props.match} />
+          </SmallColumns>
+          <NotificationsView userId={user.userID} />
+        </TripleColumn>
+      </>
+    );
+  };
 
-	render() {
-		return (
-			<>
-				{this.props.doneLoading ? (
-					<>
-						<AppBar />
-						{this.props.location.pathname !== '/home' && <ReturnToDashboardButton />}
-						<DashboardContainer>
-							<Router history={history}>
-								<Route exact path="/home" component={this.renderDashboard} />
-								<Route path="/home/profile" component={ProfileView} />
-								<Route
-									path="/home/team-member/:id"
-									render={props => (
-										<TeamMemberPageView
-											{...props}
-											userId={this.props.userProfile.user.userID}
-										/>
-									)}
-								/>
-								<Route
-									path="/home/create-team-member/"
-									render={props => (
-										<AddTeamMemberPage
-											{...props}
-											userId={this.props.userProfile.user.userID}
-										/>
-									)}
-								/>
-								<Route
-									path="/home/create-training-series"
-									render={props => (
-										<CreateTrainingSeries
-											{...props}
-											userId={this.props.userProfile.user.userID}
-										/>
-									)}
-								/>
-								<Route
-									path="/home/training-series/:id"
-									render={props => <TrainingSeriesPosts {...props} />}
-								/>
-								<Route
-									path="/home/create-post"
-									render={props => <CreatePost {...props} />}
-								/>
-								<Route
-									path="/home/assign-members/:id"
-									render={props => (
-										<AddTrainingSeriesView
-											{...props}
-											userId={this.props.userProfile.user.userID}
-										/>
-									)}
-								/>
-								<Route path="/home/post/:id" component={PostPage} />
-							</Router>
-						</DashboardContainer>
-					</>
-				) : (
-					<ProgressCircle />
-				)}
-			</>
-		);
-	}
+  render() {
+    return (
+      <>
+        {this.props.doneLoading ? (
+          <>
+            <AppBar />
+            {this.props.location.pathname !== '/home' && (
+              <ReturnToDashboardButton />
+            )}
+            <DashboardContainer>
+              <Router history={history}>
+                <Route exact path="/home" component={this.renderDashboard} />
+                <Route path="/home/profile" component={ProfileView} />
+                <Route
+                  path="/home/team-member/:id"
+                  render={props => (
+                    <TeamMemberPageView
+                      {...props}
+                      userId={this.props.userProfile.user.userID}
+                    />
+                  )}
+                />
+                <Route
+                  path="/home/create-team-member/"
+                  render={props => (
+                    <AddTeamMemberPage
+                      {...props}
+                      userId={this.props.userProfile.user.userID}
+                    />
+                  )}
+                />
+                <Route
+                  path="/home/create-training-series"
+                  render={props => (
+                    <CreateTrainingSeries
+                      {...props}
+                      userId={this.props.userProfile.user.userID}
+                    />
+                  )}
+                />
+                <Route
+                  path="/home/training-series/:id"
+                  render={props => <TrainingSeriesPosts {...props} />}
+                />
+                <Route
+                  path="/home/create-post"
+                  render={props => <CreatePost {...props} />}
+                />
+                <Route
+                  path="/home/assign-members/:id"
+                  render={props => (
+                    <AddTrainingSeriesView
+                      {...props}
+                      userId={this.props.userProfile.user.userID}
+                    />
+                  )}
+                />
+                <Route path="/home/post/:id" component={PostPage} />
+              </Router>
+            </DashboardContainer>
+          </>
+        ) : (
+          <ProgressCircle />
+        )}
+      </>
+    );
+  }
 
-	// tracking the tab value in navigation.js
-	changeTabValue = value => {
-		this.setState({
-			tabValue: value,
-		});
-	};
+  // tracking the tab value in navigation.js
+  changeTabValue = value => {
+    this.setState({
+      tabValue: value,
+    });
+  };
 }
 
 const mapStateToProps = state => {
-	return {
-		userProfile: state.userReducer.userProfile,
-		doneLoading: state.userReducer.doneLoading,
-	};
+  return {
+    userProfile: state.userReducer.userProfile,
+    doneLoading: state.userReducer.doneLoading,
+  };
 };
 
 export default connect(
-	mapStateToProps,
-	{
-		getUser,
-	}
+  mapStateToProps,
+  {
+    getUser,
+  }
 )(Authenticate(Dashboard));
 
 //Styled Components
 const TripleColumn = styled.div`
-	max-width: 1400px;
-	display: flex;
-	justify-content: space-between;
-	margin: 10px auto;
-	height: 580px;
-	@media (max-width: 1400px) {
-		flex-wrap: wrap;
-		max-width: 1000px;
-		padding: 10px;
-	}
-	@media (max-width: 768px) {
-		max-width: 768px;
-		height: 100%;
-		flex-direction: column;
-		padding: 10px;
-	}
+  max-width: 1400px;
+  display: flex;
+  justify-content: space-between;
+  margin: 10px auto;
+  height: 580px;
+  @media (max-width: 1400px) {
+    flex-wrap: wrap;
+    max-width: 1000px;
+    padding: 10px;
+  }
+  @media (max-width: 768px) {
+    max-width: 768px;
+    height: 100%;
+    flex-direction: column;
+    padding: 10px;
+  }
 `;
 const SmallColumns = styled.div`
-	display: flex;
-	width: 50%;
-	@media (max-width: 1400px) {
-		width: 100%;
-		margin-bottom: 50px;
-	}
-	@media (max-width: 768px) {
-		flex-direction: column;
-		margin-bottom: 5px;
-	}
+  display: flex;
+  width: 50%;
+  @media (max-width: 1400px) {
+    width: 100%;
+    margin-bottom: 50px;
+  }
+  @media (max-width: 768px) {
+    flex-direction: column;
+    margin-bottom: 5px;
+  }
 `;
 const DashboardContainer = styled.div`
 	display: flex;
--- client/src/components/TeamMembers/TeamMemberPageContainer/AddTeamMemberPage.js
+++ client/src/components/TeamMembers/TeamMemberPageContainer/AddTeamMemberPage.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 
 import styled from 'styled-components';
 
 // Material UI
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import NotificationWidget from './SnackBarTeamMember';
 
-import { addTeamMember } from '../../../store/actions';
+import {addTeamMember} from '../../../store/actions';
 
 const styles = theme => ({
   root: {
@@ -19,26 +19,26 @@ const styles = theme => ({
     paddingTop: theme.spacing.unit * 2,
     paddingBottom: theme.spacing.unit * 2,
     width: '80%',
-    margin: '20px auto'
+    margin: '20px auto',
   },
   form: {
     width: '90%',
-    margin: '0 auto'
+    margin: '0 auto',
   },
   info: {
-    'margin-right': '50px'
+    'margin-right': '50px',
   },
   textField: {
     marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit,
-    width: '100%'
+    width: '100%',
   },
   fab: {
-    margin: theme.spacing.unit
+    margin: theme.spacing.unit,
   },
   button: {
-    'margin-left': theme.spacing.unit
-  }
+    'margin-left': theme.spacing.unit,
+  },
 });
 
 class TeamMemberPage extends React.Component {
@@ -49,16 +49,16 @@ class TeamMemberPage extends React.Component {
       jobDescription: '',
       email: '',
       phoneNumber: '',
-      user_ID: ''
+      user_ID: '',
     },
     assignments: [],
-    trainingSeries: []
+    trainingSeries: [],
   };
 
   componentDidUpdate(prevProps) {
     if (prevProps.addSuccess !== this.props.addSuccess) {
       setTimeout(() => {
-        const { teamMemberID } = this.props.teamMember && this.props.teamMember;
+        const {teamMemberID} = this.props.teamMember && this.props.teamMember;
         this.props.history.push(`/home/team-member/${teamMemberID}`);
       }, 400);
     }
@@ -68,17 +68,16 @@ class TeamMemberPage extends React.Component {
     this.setState({
       teamMember: {
         ...this.state.teamMember,
-        [name]: event.target.value
-      }
+        [name]: event.target.value,
+      },
     });
   };
 
   addNewTeamMember = e => {
     e.preventDefault();
-
     const newMember = {
       ...this.state.teamMember,
-      user_ID: this.props.userId
+      user_ID: this.props.userId,
     };
 
     this.props.addTeamMember(newMember);
@@ -86,62 +85,49 @@ class TeamMemberPage extends React.Component {
 
   handleDate = name => event => {
     this.setState({
-      [name]: event.target.value
+      [name]: event.target.value,
     });
   };
 
   render() {
-    const { classes } = this.props;
+    const {classes} = this.props;
 
     return (
       <MainContainer>
         <Typography variant="display1" align="center" gutterBottom>
           Add A New Team Member
         </Typography>
-        <form className={classes.form}>
-          <ButtonContainer>
-            <NotificationWidget
-              teamMember={this.state.teamMember}
-              editTeamMember={this.props.editTeamMember}
-              addTeamMember={this.addNewTeamMember}
-              type="success"
-              submitType="add"
-            />
-            <Button
-              variant="contained"
-              className={classes.button}
-              onClick={e => this.props.history.push('/home')}
-            >
-              Cancel
-            </Button>
-          </ButtonContainer>
-          {/* <DeleteModal deleteType='inTeamMemberPage' id={this.props.urlId} /> */}
+        <form className={classes.form} onSubmit={e => this.addNewTeamMember(e)}>
           <Paper className={classes.root}>
             <Typography>Team Member Info</Typography>
             <MemberInfoContainer>
               <TextField
+                autoFocus="true"
                 id="standard-name"
-                label="first name"
+                label="First Name"
                 className={classes.textField}
                 value={this.state.teamMember.firstName}
                 onChange={this.handleChange('firstName')}
                 margin="normal"
+                required
               />
               <TextField
                 id="standard-name"
-                label="last name"
+                label="Last Name"
                 className={classes.textField}
                 value={this.state.teamMember.lastName}
                 onChange={this.handleChange('lastName')}
                 margin="normal"
+                required
               />
               <TextField
                 id="standard-name"
-                label="job description"
+                label="Job Description"
                 className={classes.textField}
                 value={this.state.teamMember.jobDescription}
                 onChange={this.handleChange('jobDescription')}
                 margin="normal"
+                required
               />
             </MemberInfoContainer>
           </Paper>
@@ -150,38 +136,47 @@ class TeamMemberPage extends React.Component {
             <MemberInfoContainer>
               <TextField
                 id="standard-name"
-                label="email"
+                label="Email"
                 className={classes.textField}
                 value={this.state.teamMember.email}
                 onChange={this.handleChange('email')}
                 margin="normal"
+                required
               />
               <TextField
                 id="standard-name"
-                label="phone"
+                label="Phone Number"
                 className={classes.textField}
                 value={this.state.teamMember.phoneNumber}
                 onChange={this.handleChange('phoneNumber')}
                 margin="normal"
+                required
               />
             </MemberInfoContainer>
           </Paper>
-          {/* <Paper className={classes.root}>
-            <Typography>Training Series</Typography>
-            <MemberInfoContainer>
-              <div>
-                <AddTeamMemberToTrainingSeriesModal
-                  modalType={"assignMultiple"}
-                  userId={this.props.userId}
-                  urlId={this.props.urlId}
-                  assignments={this.props.teamMember.assignments}
-                />
-              </div>
-              <TrainingSeriesContainer>
-                {trainingAssigments}
-              </TrainingSeriesContainer>
-            </MemberInfoContainer>
-          </Paper> */}
+          <ButtonContainer>
+            {/* <NotificationWidget
+              teamMember={this.state.teamMember}
+              editTeamMember={this.props.editTeamMember}
+              addTeamMember={this.addNewTeamMember}
+              type="success"
+              submitType="add"
+            /> */}
+            <Button
+              variant="contained"
+              className={classes.button}
+              type="submit"
+            >
+              Submit
+            </Button>
+            <Button
+              variant="contained"
+              className={classes.button}
+              type="submit"
+            >
+              Cancel
+            </Button>
+          </ButtonContainer>
         </form>
       </MainContainer>
     );
@@ -214,11 +209,11 @@ const ButtonContainer = styled.div`
 const mapStateToProps = state => {
   return {
     addSuccess: state.teamMembersReducer.status.addSuccess,
-    teamMember: state.teamMembersReducer.teamMember
+    teamMember: state.teamMembersReducer.teamMember,
   };
 };
 
 export default connect(
   mapStateToProps,
-  { addTeamMember }
+  {addTeamMember}
 )(withStyles(styles)(TeamMemberPage));
