d3d6c1e8cac91c43710558042ca2241997adad51
Nate Boyette <nateboyette@icloud.com>
2019-04-12 15:19:24 -0700

    Adjusting paper styles on team member page

--- client/src/components/TeamMembers/TeamMemberPageContainer/TeamMemberPage.js
+++ client/src/components/TeamMembers/TeamMemberPageContainer/TeamMemberPage.js
@@ -25,13 +25,14 @@ import { getTrainingSeries } from "../../../store/actions";
 const styles = theme => ({
   // these styles fixes the off-centering
   paper: {
-    width: "100%",
+    width: "89%",
     backgroundColor: theme.palette.background.paper,
     boxSizing: "border-box",
     boxShadow: theme.shadows[5],
-    padding: "20px 30px",
+    padding: theme.spacing.unit * 4,
     outline: "none",
-    margin: "20px auto",
+    margin: "5px auto",
+
     "@media (max-width: 768px)": {
       textAlign: "center",
       padding: "30px"
