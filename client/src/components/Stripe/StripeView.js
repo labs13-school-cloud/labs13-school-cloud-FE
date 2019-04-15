4d316acd5ba550b5c8a32b5461d43e1de57f0eb6
Mike Landers <landers.mike@gmail.com>
2019-04-15 10:53:30 -0700

    stripe variables

--- client/src/components/Stripe/StripeView.js
+++ client/src/components/Stripe/StripeView.js
@@ -11,8 +11,7 @@ const Stripe = styled.div`
 
 function StripeView(props) {
 	return (
-		// <StripeProvider apiKey="pk_test_L76yOnUDjq2cNP8heEH9MkpA00Ktyd3MYn"> {/* TEST */}
-		<StripeProvider apiKey="pk_live_rT507CtR4I4vTsTDTp4YroFc00lFrmSMgJ"> {/* LIVE */}
+		<StripeProvider apiKey={process.env.REACT_APP_STRIPE_API}>
 			<Stripe>
 				<Elements>
 					<CheckoutForm />
