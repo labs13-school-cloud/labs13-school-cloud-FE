e399b0870401a34646af2774b008f99f84e24154
Nate Boyette <nateboyette@icloud.com>
2019-04-16 15:56:44 -0700

    Adusted css transition timing

--- client/src/components/TeamMembers/TeamMember.js
+++ client/src/components/TeamMembers/TeamMember.js
@@ -34,7 +34,7 @@ const styles = {
     justifyContent: "space-between",
     alignItems: "center",
     borderBottom: "1px solid #E8E9EB",
-    transition: "background-color 0.2s",
+    transition: "background-color 0.3s",
 
     "&:hover": {
       cursor: "pointer",
--- client/src/components/TrainingSeries/TrainingSeries.js
+++ client/src/components/TrainingSeries/TrainingSeries.js
@@ -20,6 +20,7 @@ const styles = {
     justifyContent: "space-between",
     alignItems: "center",
     borderBottom: "1px solid #E8E9EB",
+    transition: "background-color 0.3s",
     "&:hover": {
       cursor: "pointer",
       backgroundColor: "whitesmoke"
