b666ff0dd10bd4a33fea66375dfcf559bd8b2bf3
Mike Landers <landers.mike@gmail.com>
2019-04-17 11:01:03 -0700

    Updated styling of pricing chart

--- client/src/components/LandingPage/Pricing.js
+++ client/src/components/LandingPage/Pricing.js
@@ -29,14 +29,22 @@ const styles = theme => ({
 		fontSize: '1rem'
 	},
 	table: {
-		minWidth: 400
-	}
+		minWidth: 400,
+		border:'none',
+		fontSize:'20px',
+	},
+	tableRow:{
+		fontSize: '18px'
+	},
+	tableCell:{
+		fontSize: '18px'
+	},
 });
 
 let id = 0;
-function createData(name, calories, fat, carbs, protein) {
+function createData(name, basic, premium, pro) {
 	id += 1;
-	return { id, name, calories, fat, carbs, protein };
+	return { id, name, basic, premium, pro };
 }
 
 const rows = [
@@ -104,30 +112,28 @@ class Pricing extends React.Component {
 							/>
 						</VideoContainer>
 					</FirstSection>
-					<Paper className={classes.root}>
+					<div className={classes.root}>
 						<Table className={classes.table}>
 							<TableHead>
-								<TableRow>
+								<TableRow className={classes.tableRow}>
 									<TableCell />
-									<TableCell align='right'>Basic</TableCell>
-									<TableCell align='right'>Premium</TableCell>
-									<TableCell align='right'>Pro</TableCell>
+									<TableCell className={classes.tableCell} align='right'>Basic</TableCell>
+									<TableCell className={classes.tableCell} align='right'>Premium</TableCell>
+									<TableCell className={classes.tableCell} align='right'>Pro</TableCell>
 								</TableRow>
 							</TableHead>
 							<TableBody>
 								{rows.map(row => (
 									<TableRow key={row.id}>
-										<TableCell component='th' scope='row'>
-											{row.name}
-										</TableCell>
-										<TableCell align='right'>{row.calories}</TableCell>
-										<TableCell align='right'>{row.fat}</TableCell>
-										<TableCell align='right'>{row.carbs}</TableCell>
+										<TableCell className={classes.tableCell} component='th' scope='row'>{row.name}</TableCell>
+										<TableCell className={classes.tableCell} align='right'>{row.basic}</TableCell>
+										<TableCell className={classes.tableCell} align='right'>{row.premium}</TableCell>
+										<TableCell className={classes.tableCell} align='right'>{row.pro}</TableCell>
 									</TableRow>
 								))}
 							</TableBody>
 						</Table>
-					</Paper>
+					</div>
 					<FooterContainer>
 						<FooterItemsContainer>
 							<h3>Team</h3>
@@ -142,29 +148,6 @@ class Pricing extends React.Component {
 	}
 }
 export default withStyles(styles)(Pricing);
-
-const Container = styled.div`
-    display:flex
-    justify-content: space-between;
-    width:100%;
-`;
-const Column = styled.div`
-    width:33%
-    height:100%;
-    /* padding:5px */
-    border: 1px solid #451476;
-`;
-const Title = styled.p`
-	font-size: 1.6rem;
-	text-align: center;
-	padding: 5px;
-	margin: 0;
-	background-color: #451476;
-	color: white;
-	border: 1px solid white;
-	position: relative;
-	top: 0;
-`;
 
 const LandingPageContainer = styled.div`
 	margin: 0 auto;
@@ -228,48 +211,6 @@ const FirstSection = styled.div`
 	display: flex;
 `;
 
-const MarketingContentContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	margin: 0 auto 100px;
-	padding: 0 20px;
-`;
-
-const MarketingSection = styled.div`
-	width: 100%;
-	display: flex;
-	margin-bottom: 40px;
-	&:nth-child(even) {
-		flex-direction: row-reverse;
-	}
-	img {
-		width: 100%;
-	}
-`;
-
-const MarketingImage = styled.div`
-	width: 50%;
-`;
-
-const MarketingContent = styled.div`
-	width: 50%;
-	padding: 0 25px;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	h3 {
-		font-size: 32px;
-		color: #451476;
-		margin-bottom: 0;
-	}
-	p {
-		font-size: 20px;
-	}
-`;
-
 const LandingPageContentContainer = styled.div`
 	width: 50%;
 	margin: 0 auto;
@@ -317,111 +258,6 @@ const VideoContainer = styled.div`
 	width: 50%;
 	@media (max-width: 1000px) {
 		display: none;
-	}
-`;
-
-const TrainingBotStoryContainer = styled.div`
-	display: flex;
-	justify-content: center;
-	flex-direction: column;
-	width: 85%;
-	margin: 40px auto;
-	background-color: #fafafa;
-	padding: 30px 20px;
-	h3 {
-		color: #451476;
-		font-size: 32px;
-		text-align: center;
-		margin: 0;
-		margin-bottom: 5px;
-	}
-	@media (max-width: 1000px) {
-		width: 100%;
-	}
-`;
-
-const StorySection = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: baseline;
-	margin: 0 auto;
-	@media (max-width: 1000px) {
-		flex-direction: column;
-	}
-`;
-const StoryContent = styled.p`
-	padding: 0 10px;
-`;
-
-const TrainingBotTestimonyContainer = styled.div`
-	display: flex;
-	justify-content: center;
-	width: 83%;
-	margin: 0 auto;
-`;
-
-const TestimonyContainer = styled.div`
-	display: flex;
-	padding: 5px;
-	flex-direction: column;
-	margin: 0 20px;
-	h4 {
-		margin: 0;
-		color: #451476;
-	}
-`;
-
-const TestimonyPerson = styled.div`
-	display: flex;
-	align-items: center;
-
-	svg {
-		color: #2699fb;
-		margin-right: 5px;
-	}
-`;
-
-const GetStartedContainer = styled.div`
-	display: flex;
-	width: 50%;
-	margin: 0 auto;
-	justify-content: center;
-	align-items: center;
-
-	@media (max-width: 700px) {
-		width: 70%;
-	}
-`;
-const IconBox = styled.div`
-	background-color: #7fc4fd;
-	width: 25%;
-
-	img {
-		width: 100%;
-		position: relative;
-		left: -50%;
-	}
-`;
-const GetStartedBox = styled.div`
-	background-color: #fafafa;
-	width: 75%;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	h4 {
-		color: #451476;
-		font-size: 24px;
-	}
-	button {
-		margin: 0 10px;
-		background-color: #7fc4fd;
-		color: white;
-		width: 100px;
-		&:hover {
-			background-color: #451476;
-			color: white;
-		}
 	}
 `;
 
