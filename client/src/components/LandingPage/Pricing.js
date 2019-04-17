a963f3fac14487f4afeb1b6c7a0f74672cb576e1
Mike Landers <landers.mike@gmail.com>
2019-04-17 13:34:07 -0700

    mobile pricing

--- client/src/components/LandingPage/Pricing.js
+++ client/src/components/LandingPage/Pricing.js
@@ -1,288 +1,451 @@
 // contains all components for landing page
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { withStyles } from '@material-ui/core/styles';
+import React from "react";
+import { Link } from "react-router-dom";
+import { withStyles } from "@material-ui/core/styles";
 
 //Styling
-import styled from 'styled-components';
-import { ArrowUpward } from '@material-ui/icons';
-import Button from '@material-ui/core/Button';
+import styled from "styled-components";
+import { ArrowUpward } from "@material-ui/icons";
+import Button from "@material-ui/core/Button";
 import {
-	Table,
-	TableBody,
-	TableCell,
-	TableHead,
-	TableRow,
-	Paper
-} from '@material-ui/core/';
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+  Paper
+} from "@material-ui/core/";
 
-import Logo from '../../img/training-bot.png';
+import Logo from "../../img/training-bot.png";
 //Auth
-import { login } from '../../Auth/Auth';
+import { login } from "../../Auth/Auth";
 
 const styles = theme => ({
-	root: {
-		width: '75%',
-		// marginTop: theme.spacing.unit * 3,
-		overflowX: 'auto',
-		margin: '0 auto',
-		fontSize: '1rem'
+  root: {
+    // width: '75%',
+    // marginTop: theme.spacing.unit * 3,
+    overflowX: "auto",
+    margin: "0 auto",
+    fontSize: "1rem",
+    "@media (max-width:800px)": {
+      display: "none"
+    }
+  },
+  table: {
+    minWidth: 400,
+    border: "none",
+    fontSize: "20px"
+  },
+  tableRow: {
+    fontSize: "18px"
+  },
+  tableCell: {
+    fontSize: "18px"
+  },
+  noBorder: {
+    borderBottom: "none"
+  },
+  selectButton: {
+    color: "white",
+    backgroundColor: "#451476",
+	margin: '10px auto 0px',
+    "&:hover": {
+      backgroundColor: "#451476",
+      color: "white"
 	},
-	table: {
-		minWidth: 400,
-		border:'none',
-		fontSize:'20px',
-	},
-	tableRow:{
-		fontSize: '18px'
-	},
-	tableCell:{
-		fontSize: '18px'
-	},
+  },
+  subCard: {
+	border: "1px solid #EBEBEB",
+	backgroundColor:'white',
+    borderRadiusTopLeft: "3px",
+	borderRadiusTopRight: "3px",
+    width: "94%",
+    minWidth: 201,
+    margin: '15px auto',
+	padding:10,
+	textAlign: "center",
+    "@media (min-width:800px)": {
+      display: "none"
+    }
+  },
+  title: {
+    textTransform: "uppercase",
+    fontWeight: 700,
+    margin: 10
+  },
+  price: {
+    fontSize: 20,
+    margin: "10px 0"
+  },
+  subPrice: {
+    fontSize: 12,
+    color: "grey"
+  }
 });
 
 let id = 0;
 function createData(name, basic, premium, pro) {
-	id += 1;
-	return { id, name, basic, premium, pro };
+  id += 1;
+  return { id, name, basic, premium, pro };
 }
 
 const rows = [
-	createData(
-		'Automated Text/Email',
-		<span>&#10004;</span>,
-		<span>&#10004;</span>,
-		<span>&#10004;</span>
-	),
-	createData(
-		'Unlimited Training Series',
-		<span>&#10004;</span>,
-		<span>&#10004;</span>,
-		<span>&#10004;</span>
-	),
-	createData(
-		'Unlimited Team Members',
-		<span>&#10004;</span>,
-		<span>&#10004;</span>,
-		<span>&#10004;</span>
-	),
-	createData('Message Limit', 50, 200, 1000)
+  createData(
+    "Automated Text/Email",
+    <span>&#10004;</span>,
+    <span>&#10004;</span>,
+    <span>&#10004;</span>
+  ),
+  createData(
+    "Unlimited Training Series",
+    <span>&#10004;</span>,
+    <span>&#10004;</span>,
+    <span>&#10004;</span>
+  ),
+  createData(
+    "Unlimited Team Members",
+    <span>&#10004;</span>,
+    <span>&#10004;</span>,
+    <span>&#10004;</span>
+  ),
+  createData("Message Limit", "50/mo", "200/mo", "1000/mo")
 ];
 class Pricing extends React.Component {
-	render() {
-		const { classes } = this.props;
-		return (
-			<>
-				<LandingPageContainer>
-					{/* NAVIGATION */}
-					<NavbarContainer>
-						<Link to='/'>
-							<img src={Logo} alt='A cute, personable robot' />
-						</Link>
-						<NavbarItemsContainer>
-							<NavbarItem>Team</NavbarItem>
-							<NavbarItem to='/pricing'>Pricing</NavbarItem>
-							<NavbarItem>Blog</NavbarItem>
-							<h2 onClick={login}>Sign In</h2>
-						</NavbarItemsContainer>
-					</NavbarContainer>
-					{/* JUMBOTRON STYLED SECTION */}
-					<FirstSection>
-						<LandingPageContentContainer>
-							<h1>Training Bot</h1>
-							<p>
-								"Empowers team leaders with tools to assist with their team’s
-								continual learning by sending automated Text messages/emails on
-								a scheduled interval to team members”.
-							</p>
-							<LandingPageButtonContainer>
-								<Button onClick={login}>Get Started</Button>
-								<Button variant='outlined'>Learn More</Button>
-							</LandingPageButtonContainer>
-						</LandingPageContentContainer>
-						<VideoContainer>
-							<iframe
-								width='100%'
-								height='400'
-								src='https://www.youtube.com/embed/CQ85sUNBK7w'
-								frameborder='0'
-								allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
-								allowfullscreen
-								title='Marketing Video'
-							/>
-						</VideoContainer>
-					</FirstSection>
-					<div className={classes.root}>
-						<Table className={classes.table}>
-							<TableHead>
-								<TableRow className={classes.tableRow}>
-									<TableCell />
-									<TableCell className={classes.tableCell} align='right'>Basic</TableCell>
-									<TableCell className={classes.tableCell} align='right'>Premium</TableCell>
-									<TableCell className={classes.tableCell} align='right'>Pro</TableCell>
-								</TableRow>
-							</TableHead>
-							<TableBody>
-								{rows.map(row => (
-									<TableRow key={row.id}>
-										<TableCell className={classes.tableCell} component='th' scope='row'>{row.name}</TableCell>
-										<TableCell className={classes.tableCell} align='right'>{row.basic}</TableCell>
-										<TableCell className={classes.tableCell} align='right'>{row.premium}</TableCell>
-										<TableCell className={classes.tableCell} align='right'>{row.pro}</TableCell>
-									</TableRow>
-								))}
-							</TableBody>
-						</Table>
-					</div>
-					<FooterContainer>
-						<FooterItemsContainer>
-							<h3>Team</h3>
-							<h3>Pricing</h3>
-							<h3>Blog</h3>
-						</FooterItemsContainer>
-						<ArrowUpward onClick={() => window.scrollTo(0, 0)} />
-					</FooterContainer>
-				</LandingPageContainer>
-			</>
-		);
-	}
+  render() {
+    const { classes } = this.props;
+    return (
+      <LandingPageContainer>
+        {/* NAVIGATION */}
+        <NavbarContainer>
+          <Link to="/">
+            <img src={Logo} alt="A cute, personable robot" />
+          </Link>
+          <NavbarItemsContainer>
+            <NavbarItem>Team</NavbarItem>
+            <NavbarItem to="/pricing">Pricing</NavbarItem>
+            <NavbarItem>Blog</NavbarItem>
+            <h2 onClick={login}>Sign In</h2>
+          </NavbarItemsContainer>
+        </NavbarContainer>
+        {/* JUMBOTRON STYLED SECTION */}
+        <FirstSection>
+          <Typography variant="h3" style={{ paddingLeft: "10%" }}>
+            Pricing
+          </Typography>
+          <div className={classes.root}>
+            <Table className={classes.table}>
+              <TableHead>
+                <TableRow className={classes.tableRow}>
+                  <TableCell />
+                  <TableCell className={classes.tableCell} align="center">
+                    Basic
+                  </TableCell>
+                  <TableCell className={classes.tableCell} align="center">
+                    Premium
+                  </TableCell>
+                  <TableCell className={classes.tableCell} align="center">
+                    Pro
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map(row => (
+                  <TableRow key={row.id}>
+                    <TableCell
+                      className={classes.tableCell}
+                      component="th"
+                      scope="row"
+                    >
+                      {row.name}
+                    </TableCell>
+                    <TableCell className={classes.tableCell} align="center">
+                      {row.basic}
+                    </TableCell>
+                    <TableCell className={classes.tableCell} align="center">
+                      {row.premium}
+                    </TableCell>
+                    <TableCell className={classes.tableCell} align="center">
+                      {row.pro}
+                    </TableCell>
+                  </TableRow>
+                ))}
+                <TableRow>
+                  <TableCell className={classes.noBorder} align="center" />
+                  <TableCell className={classes.noBorder} align="center">
+                    <Button
+                      variant="container"
+                      className={classes.selectButton}
+                      onClick={login}
+                    >
+                      Sign Up
+                    </Button>
+                  </TableCell>
+                  <TableCell className={classes.noBorder} align="center">
+                    <Button
+                      variant="container"
+                      className={classes.selectButton}
+                      onClick={login}
+                    >
+                      Sign Up
+                    </Button>
+                  </TableCell>
+                  <TableCell className={classes.noBorder} align="center">
+                    <Button
+                      variant="container"
+                      className={classes.selectButton}
+                      onClick={login}
+                    >
+                      Sign Up
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </div>
+          <div className={classes.subCard}>
+            <Typography className={classes.title}>Basic</Typography>
+            <Typography className={classes.price}>FREE</Typography>
+            <div className={classes.content}>
+              <Typography className={classes.feature}>
+                Automated Text/Email
+              </Typography>
+              <Typography className={classes.feature}>
+                Unlimited Training Series
+              </Typography>
+              <Typography className={classes.feature}>
+                Unlimited Team Members
+              </Typography>
+              <div className={classes.spread}>
+                <Typography className={classes.feature}>
+                  Message Limit
+                </Typography>
+                <Typography className={classes.feature}>50 / mo</Typography>
+              </div>
+            </div>
+            <Button
+              variant="container"
+              className={classes.selectButton}
+              onClick={login}
+            >
+              Sign Up
+            </Button>{" "}
+          </div>
+          <div className={classes.subCard}>
+            <Typography className={classes.title}>Premium</Typography>
+            <Typography className={classes.price}>
+              $5
+              <span className={classes.subPrice}> / mo</span>
+            </Typography>{" "}
+            <div className={classes.content}>
+              <Typography className={classes.feature}>
+                Automated Text/Email
+              </Typography>
+              <Typography className={classes.feature}>
+                Unlimited Training Series
+              </Typography>
+              <Typography className={classes.feature}>
+                Unlimited Team Members
+              </Typography>
+              <div className={classes.spread}>
+                <Typography className={classes.feature}>
+                  Message Limit
+                </Typography>
+                <Typography className={classes.feature}>200 / mo</Typography>
+              </div>
+            </div>
+            <Button
+              variant="container"
+              className={classes.selectButton}
+              onClick={login}
+            >
+              Sign Up
+            </Button>{" "}
+          </div>
+          <div className={classes.subCard}>
+            <Typography className={classes.title}>Pro</Typography>
+            <Typography className={classes.price}>
+              $10
+              <span className={classes.subPrice}> / mo</span>
+            </Typography>{" "}
+            <div className={classes.content}>
+              <Typography className={classes.feature}>
+                Automated Text/Email
+              </Typography>
+              <Typography className={classes.feature}>
+                Unlimited Training Series
+              </Typography>
+              <Typography className={classes.feature}>
+                Unlimited Team Members
+              </Typography>
+              <div className={classes.spread}>
+                <Typography className={classes.feature}>
+                  Message Limit
+                </Typography>
+                <Typography className={classes.feature}>1000 / mo</Typography>
+              </div>
+            </div>
+            <Button
+              variant="container"
+              className={classes.selectButton}
+              onClick={login}
+            >
+              Sign Up
+            </Button>{" "}
+          </div>
+        </FirstSection>
+        <FooterContainer>
+          <FooterItemsContainer>
+            <h3>Team</h3>
+            <h3>Pricing</h3>
+            <h3>Blog</h3>
+          </FooterItemsContainer>
+          <ArrowUpward onClick={() => window.scrollTo(0, 0)} />
+        </FooterContainer>
+      </LandingPageContainer>
+    );
+  }
 }
 export default withStyles(styles)(Pricing);
 
 const LandingPageContainer = styled.div`
-	margin: 0 auto;
-	width: 100%;
-	max-width: 1280px;
-	background-color: white;
+  margin: 0 auto;
+  width: 100%;
+  height: 100vh;
+  max-width: 1280px;
+  background-color: white;
 `;
 
 const NavbarContainer = styled.nav`
-	height: 75px;
-	background-color: white;
-	width: 100%;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	border-bottom: 1px solid #f0f4f8;
-	padding: 0 25px;
-	box-sizing: border-box;
-	img {
-		width: 50px;
-	}
-	h2,
-	h3 {
-		margin-left: 30px;
-		font-size: 16px;
-		font-weight: 500;
-		cursor: pointer;
-	}
-	h2 {
-		color: #451476;
-		border: 1px solid #451476;
-		background-color: white;
-		padding: 8px;
-		border-radius: 7%;
-		&:hover {
-			background-color: #451476;
-			color: white;
-		}
-	}
-	h3 {
-		color: #451476;
-	}
+  height: 75px;
+  background-color: white;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid #f0f4f8;
+  padding: 0 25px;
+  box-sizing: border-box;
+  img {
+    width: 50px;
+  }
+  h2,
+  h3 {
+    margin-left: 30px;
+    font-size: 16px;
+    font-weight: 500;
+    cursor: pointer;
+  }
+  h2 {
+    color: #451476;
+    border: 1px solid #451476;
+    background-color: white;
+    padding: 8px;
+    border-radius: 7%;
+    &:hover {
+      background-color: #451476;
+      color: white;
+    }
+  }
+  h3 {
+    color: #451476;
+  }
 `;
 const NavbarItemsContainer = styled.div`
-	display: flex;
-	align-items: center;
+  display: flex;
+  align-items: center;
 `;
 const NavbarItem = styled(Link)`
-	margin-left: 30px;
-	font-size: 16px;
-	font-weight: 500;
-	text-decoration: none;
-	cursor: pointer;
+  margin-left: 30px;
+  font-size: 16px;
+  font-weight: 500;
+  text-decoration: none;
+  cursor: pointer;
 `;
 
 const FirstSection = styled.div`
-	background-color: #fafafa;
-	width: 95%;
-	margin: 0px auto 20px;
-	padding: 50px 10px;
-	display: flex;
+  background-color: #fafafa;
+  width: 95%;
+  margin: 5% auto 20px;
+  padding: 50px 10px;
+  display: flex;
+  flex-direction: column;
 `;
 
 const LandingPageContentContainer = styled.div`
-	width: 50%;
-	margin: 0 auto;
+  width: 50%;
+  margin: 0 auto;
 
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	padding: 0 80px;
-	h1 {
-		margin-top: 0;
-		color: #451476;
-		font-size: 42px;
-	}
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0 80px;
+  h1 {
+    margin-top: 0;
+    color: #451476;
+    font-size: 42px;
+  }
 
-	@media (max-width: 1000px) {
-		width: 100%;
-		padding: 0;
-		text-align: center;
-		p {
-			padding: 0 50px;
-		}
-	}
+  @media (max-width: 1000px) {
+    width: 100%;
+    padding: 0;
+    text-align: center;
+    p {
+      padding: 0 50px;
+    }
+  }
 `;
 
 const LandingPageButtonContainer = styled.div`
-	display: flex;
-	margin-top: 30px;
-	button:first-child {
-		margin: 0 10px;
-		background-color: #451476;
-		color: white;
-	}
-	button:nth-child(2) {
-		border: 1px solid #451476;
-		color: #451476;
-		&:hover {
-			background-color: #451476;
-			color: white;
-		}
-	}
+  display: flex;
+  margin-top: 30px;
+  button:first-child {
+    margin: 0 10px;
+    background-color: #451476;
+    color: white;
+  }
+  button:nth-child(2) {
+    border: 1px solid #451476;
+    color: #451476;
+    &:hover {
+      background-color: #451476;
+      color: white;
+    }
+  }
 `;
 
 const VideoContainer = styled.div`
-	width: 50%;
-	@media (max-width: 1000px) {
-		display: none;
-	}
+  width: 50%;
+  @media (max-width: 1000px) {
+    display: none;
+  }
 `;
 
 const FooterContainer = styled.div`
-	width: 100%;
-	display: flex;
-	align-items: center;
-	margin-top: 100px;
-	svg {
-		margin: 0 auto;
-		font-size: 30px;
-		cursor: pointer;
-	}
+  width: 100%;
+  display: flex;
+  align-items: center;
+  margin-top: 100px;
+  position: sticky;
+  top: 100%;
+  svg {
+    margin: 0 auto;
+    font-size: 30px;
+    cursor: pointer;
+  }
 `;
 
 const FooterItemsContainer = styled.div`
-	background-color: #451476;
-	display: flex;
-	color: white;
-	justify-content: center;
-	width: 90%;
-	h3 {
-		font-size: 16px;
-		font-weight: 500;
-		padding: 0 20px;
-		cursor: pointer;
-	}
+  background-color: #451476;
+  display: flex;
+  color: white;
+  justify-content: center;
+  width: 90%;
+  h3 {
+    font-size: 16px;
+    font-weight: 500;
+    padding: 0 20px;
+    cursor: pointer;
+  }
 `;
