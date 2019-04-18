95da65dbe4ee98c67c130d67d4d29852f9739bfe
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-04-17 20:40:41 -0400

    Add more attributes to form and inputs

--- client/src/components/LandingPage/Team.js
+++ client/src/components/LandingPage/Team.js
@@ -156,23 +156,27 @@ class Team extends React.Component {
               name="contact"
               method="POST"
               netlify
+              enctype="application/x-www-form-urlencoded"
             >
               <TextField
                 label="Name"
                 className={classes.textField}
                 margin="normal"
+                name="contact-name"
                 required
               />
               <TextField
                 label="Email"
                 className={classes.textField}
                 margin="normal"
+                name="contact-email"
                 required
               />
               <TextField
                 label="Message"
                 className={classes.textField}
                 margin="normal"
+                name="contact-message"
                 multiline
                 rows="8"
                 placeholder="Type your message here"
