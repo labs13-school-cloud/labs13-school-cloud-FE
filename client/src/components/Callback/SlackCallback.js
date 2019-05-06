5c727280ab85113a289f075828ad245ebdff0941
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-05-06 16:10:18 -0400

    Added query-strings to parse Slack returned data

--- client/src/components/Callback/SlackCallback.js
+++ client/src/components/Callback/SlackCallback.js
@@ -3,18 +3,18 @@ import qs from "query-string";
 import axios from "axios";
 
 function SlackCallback(props) {
-  useEffect(() => {
-    // componentDidMount
-    const { search } = props.location;
-    const code = qs.parse(props.location.search, {
-      ignoreQueryPrefix: true
-    }).code;
-    const state = qs.parse(props.location.search, {
-      ignoreQueryPrefix: true
-    }).state;
+  //useEffect(() => {
+  // componentDidMount
+  const { search } = props.location;
+  // const code = qs.parse(search, {
+  //   ignoreQueryPrefix: true
+  // }).code;
+  // const state = qs.parse(search, {
+  //   ignoreQueryPrefix: true
+  // }).state;
 
-    axios.post(`${process.env.REACT_APP_API}/api/slack/oauth`, { code });
-  }, []);
+  //   axios.post(`${process.env.REACT_APP_API}/api/slack/oauth`, { code });
+  // }, []);
 
   return <div>Authorizing Slack, please wait...</div>;
 }
