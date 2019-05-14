12571e76e853eb62b235e6c77242d3939b76a52e
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-05-13 21:24:30 -0400

    Reworks messages actions to remove unused actions/creators and add a getAll action creator

--- src/store/actions/messagesActions.js
+++ src/store/actions/messagesActions.js
@@ -1,63 +1,36 @@
 import axios from "axios";
-import history from "../../history.js";
-// GET MESSAGES
+import history from "history.js";
+
 export const GET_MESSAGES_START = "GET_MESSAGES_START";
 export const GET_MESSAGES_SUCCESS = "GET_MESSAGES_SUCCESS";
 export const GET_MESSAGES_FAIL = "GET_MESSAGES_FAIL";
 
-// GET SINGLE MESSAGE
-export const GET_SINGLE_MESSAGE_START = "GET_SINGLE_MESSAGE_START";
-export const GET_SINGLE_MESSAGE_SUCCESS = "GET_SINGLE_MESSAGE_SUCCESS";
-export const GET_SINGLE_MESSAGE_FAIL = "GET_SINGLE_MESSAGE_FAIL";
-
-// ADD A NEW MESSAGE
 export const ADD_MESSAGE_START = "ADD_MESSAGE_START";
 export const ADD_MESSAGE_SUCCESS = "ADD_MESSAGE_SUCCESS";
 export const ADD_MESSAGE_FAIL = "ADD_MESSAGE_FAIL";
 
-// EDIT A MESSAGE
 export const EDIT_MESSAGE_START = "EDIT_MESSAGE_START";
 export const EDIT_MESSAGE_SUCCESS = "EDIT_MESSAGE_SUCCESS";
 export const EDIT_MESSAGE_FAIL = "EDIT_MESSAGE_FAIL";
 
-// DELETE A MESSAGE
 export const DELETE_MESSAGE_START = "DELETE_MESSAGE_START";
 export const DELETE_MESSAGE_SUCCESS = "DELETE_MESSAGE_SUCCESS";
 export const DELETE_MESSAGE_FAIL = "DELETE_MESSAGE_FAIL";
 
 // GET all messages for a training series
 
-export const getTrainingSeriesMessages = id => dispatch => {
-  dispatch({
-    type: GET_MESSAGES_START
-  });
+export const getAllMessages = () => dispatch => {
+  dispatch({ type: GET_MESSAGES_START });
   axios
-    .get(`${process.env.REACT_APP_API}/api/training-series/${id}/messages`) //endpoint validated
+    .get(`${process.env.REACT_APP_API}/api/messages`)
     .then(res => {
-      dispatch({
-        type: GET_MESSAGES_SUCCESS,
-        payload: res.data // should include ".trainingSeries and .messages"
-      });
+      dispatch({ type: GET_MESSAGES_SUCCESS, payload: res.data.messages });
     })
     .catch(err => {
       dispatch({ type: GET_MESSAGES_FAIL, error: err });
     });
 };
 
-//GET a single message by id
-export const getMessageById = id => dispatch => {
-  dispatch({ type: GET_SINGLE_MESSAGE_START });
-  axios
-    .get(`${process.env.REACT_APP_API}/api/messages/${id}`) //endpoint validated
-    .then(
-      res =>
-        dispatch({
-          type: GET_SINGLE_MESSAGE_SUCCESS,
-          payload: res.data.message
-        }) //response validated
-    )
-    .catch(err => dispatch({ type: GET_SINGLE_MESSAGE_FAIL, error: err }));
-};
 // POST a new message
 export const createAMessage = (message, trainingSeriesID) => dispatch => {
   dispatch({ type: ADD_MESSAGE_START });
@@ -65,13 +38,11 @@ export const createAMessage = (message, trainingSeriesID) => dispatch => {
     .post(`${process.env.REACT_APP_API}/api/messages`, message) //endpoint validated
     .then(res => {
       dispatch({ type: ADD_MESSAGE_SUCCESS, payload: res.data.newMessage }); //response is correct
+      history.push(`/home/training-series/${trainingSeriesID}`);
     })
-    .catch(err => dispatch({ type: ADD_MESSAGE_FAIL, error: err }))
-    .finally(() => {
-      // dispatch(getTrainingSeriesMessages(trainingSeriesID))
-      history.push(`/home/training-series/${trainingSeriesID}`); //potentially why the routing on TS messes up?
-    });
+    .catch(err => dispatch({ type: ADD_MESSAGE_FAIL, error: err }));
 };
+
 // PUT a message
 export const editMessage = (id, updates) => dispatch => {
   //console.log(updates)
@@ -87,6 +58,7 @@ export const editMessage = (id, updates) => dispatch => {
     )
     .catch(err => dispatch({ type: EDIT_MESSAGE_FAIL, error: err }));
 };
+
 // DELETE a message
 export const deleteMessage = id => dispatch => {
   dispatch({ type: DELETE_MESSAGE_START });
