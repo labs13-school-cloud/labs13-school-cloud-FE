ed1a655e6d62ea374b16b2471c435ae068a9a3d2
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-05-16 12:04:39 -0400

    Allows the Add/Edit Team Member component to be more flexible in its size/organization

---
+++ src/components/Pages/TeamMembers/Edit/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const EditWrapper = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+  margin: auto;
+  max-width: 1600px;
+`;
