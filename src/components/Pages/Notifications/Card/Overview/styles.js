38b26445f1ca881298300c842596002c97613878
Thomas Hessburg <44884308+TomHessburg@users.noreply.github.com>
2019-05-16 18:55:43 -0400

    :bug: team member tab layoung matchet training series
    adjusted styles on team members tab to match styles on training series tab

--- src/components/Pages/Notifications/Card/Overview/styles.js
+++ src/components/Pages/Notifications/Card/Overview/styles.js
@@ -11,12 +11,17 @@ export const ListStyles = styled.ul`
 export const styles = {
   listItem: {
     width: "100%",
-    marginBottom: 10,
-    marginTop: 4,
+    paddingBottom: 10,
+    paddingTop: 4,
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
-    borderBottom: "1px solid #E8E9EB"
+    borderBottom: "1px solid #E8E9EB",
+    transition: "background-color 0.3s",
+    "&:hover": {
+      cursor: "pointer",
+      backgroundColor: "whitesmoke"
+    }
   },
   title: {
     fontSize: 16
