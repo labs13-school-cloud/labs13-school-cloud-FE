b4769673c84c3ca7e57a9d840e05b6b7f4e71585
Thomas Hessburg <44884308+TomHessburg@users.noreply.github.com>
2019-05-20 16:51:58 -0400

    :sparkles: new desktop navigation
    this commit adds a new full screen navigation menu

---
+++ src/components/Pages/Dashboard/Dashboard/helpers/DesktopNavigation.js
@@ -0,0 +1,60 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+
+import styled from "styled-components";
+
+class SimpleTabs extends React.Component {
+  state = {
+    value: 0
+  };
+
+  handleChange = (event, value) => {
+    value === 0
+      ? this.props.setTopTab("overview")
+      : value === 1
+      ? this.props.setTopTab("team members")
+      : value === 2
+      ? this.props.setTopTab("training series")
+      : value === 3
+      ? this.props.setTopTab("notifications")
+      : this.props.setTopTab("responses");
+
+    this.setState({
+      value
+    });
+  };
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <div>
+        <AppBar position="static">
+          <TabsStyled value={value} onChange={this.handleChange}>
+            <Tab label="Overview" />
+            <Tab label="Team Members" />
+            <Tab label="Training Series" />
+            <Tab label="Notifications" />
+            <Tab label="Responses" />
+          </TabsStyled>
+        </AppBar>
+      </div>
+    );
+  }
+}
+export default SimpleTabs;
+
+const TabsStyled = styled(Tabs)`
+  background: #441476;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+`;
+
+const Wrapper = styled(AppBar)`
+  @media (max-width: 650px) {
+    display: none;
+  }
+`;
