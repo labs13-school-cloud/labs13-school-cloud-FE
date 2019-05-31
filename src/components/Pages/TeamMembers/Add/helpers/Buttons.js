62111d837a57d6a1ca3e19991c1545c6722e1d29
Brandon Vilaychith <blvilaychith@gmail.com>
2019-05-31 16:52:07 -0500

    Fixed cancel button bug

--- src/components/Pages/TeamMembers/Add/helpers/Buttons.js
+++ src/components/Pages/TeamMembers/Add/helpers/Buttons.js
@@ -8,42 +8,47 @@ import Snackbar from "components/UI/Snackbar/Snackbar.js";
 import { styles, ButtonContainer, LoadingImage } from "../styles.js";
 import { withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
+import { withRouter } from 'react-router-dom';
 
-function Buttons({ classes, state, status }) {
-  return (
-    <ButtonContainer>
-      <Button
-        disabled={state.isRouting || state.buttonDisabled}
-        variant="contained"
-        className={classes.addButton}
-        type="submit"
-      >
-        {state.isRouting ? (
-          <LoadingImage src={TrainingBotGIF} alt="Loading Icon" />
-        ) : status === "add" ? (
-          "Add Member"
-        ) : (
-          "Save"
-        )}
-      </Button>
-      {status === "add" ? (
-        <Button className={classes.button}>Cancel</Button>
-      ) : (
-        <DeleteModal
-          deleteType="inTeamMemberPage"
-          teamMemberId={state.teamMember.id}
-          user_id={state.teamMember.user_id}
-          displayType="button"
-        />
-      )}
-      {state.snackbar && (
-        <Snackbar
-          message={`Your team member have been successfully ${status}ed.`}
-          type="success"
-        />
-      )}
-    </ButtonContainer>
-  );
+function Buttons({ classes, state, status, history }) {
+	const handleCancel = e => {
+		e.preventDefault();
+		history.push('/home')
+	};
+
+	return (
+		<ButtonContainer>
+			<Button
+				disabled={state.isRouting || state.buttonDisabled}
+				variant="contained"
+				className={classes.addButton}
+				type="submit">
+				{state.isRouting ? (
+					<LoadingImage src={TrainingBotGIF} alt="Loading Icon" />
+				) : status === "add" ? (
+					"Add Member"
+				) : (
+					"Save"
+				)}
+			</Button>
+			{status === "add" ? (
+				<Button className={classes.button} onClick={handleCancel}>Cancel</Button>
+			) : (
+				<DeleteModal
+					deleteType="inTeamMemberPage"
+					teamMemberId={state.teamMember.id}
+					user_id={state.teamMember.user_id}
+					displayType="button"
+				/>
+			)}
+			{state.snackbar && (
+				<Snackbar
+					message={`Your team member have been successfully ${status}ed.`}
+					type="success"
+				/>
+			)}
+		</ButtonContainer>
+	);
 }
 
-export default withStyles(styles)(Buttons);
+export default withRouter(withStyles(styles)(Buttons));
