da723a92a8240856ef0e8d30453eeaec9e3a2bbf
Brandon Vilaychith <blvilaychith@gmail.com>
2019-06-07 11:49:06 -0500

    removed parameter

--- src/store/actions/messagesActions.js
+++ src/store/actions/messagesActions.js
@@ -19,7 +19,7 @@ export const DELETE_MESSAGE_FAIL = "DELETE_MESSAGE_FAIL";
 
 // GET all messages for a training series
 
-export const getAllMessages = (id) => dispatch => {
+export const getAllMessages = () => dispatch => {
   dispatch({ type: GET_MESSAGES_START });
   axios
     .get(`${process.env.REACT_APP_API}/api/messages`)
