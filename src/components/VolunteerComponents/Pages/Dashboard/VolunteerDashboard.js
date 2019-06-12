dfedee49898b344067ff35fd2e6a9e7efcea1e03
Brandon Vilaychith <blvilaychith@gmail.com>
2019-06-12 00:13:59 -0500

    Added comment

--- src/components/VolunteerComponents/Pages/Dashboard/VolunteerDashboard.js
+++ src/components/VolunteerComponents/Pages/Dashboard/VolunteerDashboard.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+/**
+ * This is where we will set up our volunteer routes 
+ * and connect all of our components regarding
+ * volunteers
+ */
+
 const VolunteerDashboard = (props) => {
     return (
         <h1>Volunteer Dashboard</h1>
