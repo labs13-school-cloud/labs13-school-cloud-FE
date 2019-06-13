ab3fba8f39734da8304a46bef11b96e04f08a637
Stephanie Patton <stephaniepatton@Stephanies-MacBook-Air.local>
2019-06-13 10:47:36 -0500

    adding training series files to new dashboard

--- src/components/VolunteerComponents/Pages/Dashboard/VolunteerDashboard.js
+++ src/components/VolunteerComponents/Pages/Dashboard/VolunteerDashboard.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
 /**
- * This is where we will set up our volunteer routes 
+ * This is where we will set up our volunteer routes
  * and connect all of our components regarding
  * volunteers
  */
 
-const VolunteerDashboard = (props) => {
-    return (
-        <h1>Volunteer Dashboard</h1>
-    )
-}
+const VolunteerDashboard = props => {
+  return (
+    
+  );
+};
 
 export default VolunteerDashboard;
