a26c8f27248ebd42bb9397978f13edc8c9f49969
“Josefia <josefia.magagnoli@gmail.com>
2019-06-19 15:29:23 -0400

    Volunteer actions

---
+++ src/components/Pages/Volunteers/List/Tab/Tab.js
@@ -0,0 +1,85 @@
+// Main page fro displaying a list of all classes
+import React, { useEffect } from  "react";
+import { connect } from  "react-redux";
+
+
+import history from "history.js";
+
+import DeleteModal from "../../../../UI/Modals/deleteModal";
+
+import { getClassList, deleteClass, addClass } from "../../../../../store/actions/classesActions";
+
+import { Typography } from "@material-ui/core/";
+import { ListStyles } from "./styles.js";
+
+
+function  Overview(props) {
+    useEffect(() => {
+        props.getClassList()
+    }, [getClassList]);
+
+
+    console.log(props)
+    return (
+        <div style={{ display: "flex", flexWrap: "wrap", alignItems: "flex-end" }}>
+            {props.getFiltered(props.classList).map(
+                ({ id, class_name, subject, grade_level, number_of_students, teacher_name }) => {
+                    console.log(id)
+                    return (
+                        <ListStyles key={id} component="li" className={props.classList.listItem}>
+                         
+                            <Typography key={id}>
+                                <div 
+                                style={{ cursor: "pointer" }}
+                                >
+                                    <Typography variant="subtitle1" style={{ display:"flex", justifyContent:"space-between" }}>
+                                        {class_name}
+                                    <DeleteModal 
+                                        deleteType="classes"
+                                        classId={id}
+                                        className={`material-icons ${props.classList.icons}`}
+                                        style={{ zIndex: "1000" }}
+                                    />
+                                    </Typography>
+                                    <div onClick={() => history.push(`home/classes/${id}`)}>
+                                        
+                                        <hr />
+                                        <Typography variant="overline">
+                                            {`Subject: ${subject}`}
+                                        </Typography>
+                                        <Typography variant="overline">
+                                            {`Grade Level: ${grade_level}`}
+                                        </Typography>
+                                        <Typography variant="overline">
+                                            {`Teacher Name: ${teacher_name}`}
+                                        </Typography>
+                                        <Typography variant="overline">
+                                            {`Number Of Students: ${number_of_students}`}
+                                        </Typography>
+                                    </div>
+
+                                </div>
+                            </Typography>
+                        </ListStyles>
+                    )
+                 } 
+            )}
+            <AddClassModal 
+            className={`material-icons ${props.classList.icons}`}
+            
+            />
+
+        </div>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+      classList: state.classesReducer.classList
+    }
+};
+
+export default connect(
+    mapStateToProps,
+    { getClassList, deleteClass, addClass }
+)(Overview);
