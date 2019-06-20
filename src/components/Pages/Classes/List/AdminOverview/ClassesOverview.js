80e26c559fd1383f27f76fb7094d70e64233b9ce
Brandon <brandon@pop-os.localdomain>
2019-06-20 11:50:43 -0500

    added edit modal

--- src/components/Pages/Classes/List/AdminOverview/ClassesOverview.js
+++ src/components/Pages/Classes/List/AdminOverview/ClassesOverview.js
@@ -3,7 +3,6 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 
-
 // Redux actions imports
 import { getClassList } from "store/actions";
 
@@ -11,20 +10,30 @@ import { getClassList } from "store/actions";
 import { withStyles } from "@material-ui/core/styles";
 import { ListItem, ListItemText } from "@material-ui/core/";
 import DeleteModal from "components/UI/Modals/deleteModal";
+import EditModal from "components/UI/Modals/editModal";
 // Uses styles from training series overview
-import { ListStyles, styles } from "components/Pages/TrainingSeries/List/Overview/styles.js";
+import {
+  ListStyles,
+  styles
+} from "components/Pages/TrainingSeries/List/Overview/styles.js";
 
 function ClassesOverview(props) {
   const { getFiltered, classes, history, getClassList, classList } = props;
-  
+
   useEffect(() => {
     getClassList();
   }, [getClassList]);
 
   return (
     <ListStyles>
-      {getFiltered(classList).map(props => {
-        const { id, class_name, subject, grade_level, number_of_students } = props;
+      {getFiltered(classList).map(classSingle => {
+        const {
+          id,
+          class_name,
+          subject,
+          grade_level,
+          number_of_students
+        } = classSingle;
         return (
           <ListItem key={id} component="li" className={classes.listItem}>
             <ListItemText
@@ -32,11 +41,24 @@ function ClassesOverview(props) {
               secondary={`Subject: ${subject} | Grade: ${grade_level} | Students: ${number_of_students}`}
               onClick={e => history.push(`/home/training-series/${id}`)}
             />
-            <DeleteModal
-              deleteType="classes"
-              classId={props.id}
-              className={`material-icons ${classes.icons}`}
-            />
+
+            <div
+              style={{
+                width: "65px",
+                display: "flex",
+                justifyContent: "space-between"
+              }}
+            >
+              <DeleteModal
+                deleteType="classes"
+                classId={props.id}
+                className={`material-icons ${classes.icons}`}
+              />
+              <EditModal
+                classList={classSingle}
+                updateType="classes"
+              />
+            </div>
           </ListItem>
         );
       })}
@@ -45,9 +67,9 @@ function ClassesOverview(props) {
 }
 
 const mapStateToProps = state => {
-  return { 
+  return {
     classList: state.classesReducer.classList
-  }
+  };
 };
 
 export default connect(
