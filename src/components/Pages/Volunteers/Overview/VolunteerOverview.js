c3f7b8dd7b962953d113c28d2b39cc85bd3a9d83
Brandon <brandon@pop-os.localdomain>
2019-06-19 22:45:54 -0500

    Volunteer overview updated

--- src/components/Pages/Volunteers/Overview/VolunteerOverview.js
+++ src/components/Pages/Volunteers/Overview/VolunteerOverview.js
@@ -1,82 +1,71 @@
 // Main page fro displaying a list of all classes
-import React, { useEffect } from  "react";
-import { connect } from  "react-redux";
-
-import styled from "styled-components";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 
 import history from "history.js";
 
+import { getVolunteers } from "store/actions";
+// Material UI imports
 import DeleteModal from "components/UI/Modals/deleteModal";
-import { getClassList, deleteClass } from "store/actions";
-
 import { withStyles } from "@material-ui/core/styles";
-import { ListItem, ListItemText } from "@material-ui/core/";
 import { ListStyles } from "./styles.js";
 import { Typography } from "@material-ui/core/";
 
-function  Overview(props) {
-    useEffect(() => {
-        props.getClassList()
-    }, [props.getClassList]);
+function Overview(props) {
+  const { getVolunteers, volunteers, getFiltered, classes } = props;
 
-    return (
-        <div style={{ display: "flex", flexWrap: "wrap" }}>
-            {props.getFiltered(props.classList).map(
-                ({ id, class_name, subject, grade_level, number_of_students, teacher_name }) => {
-                    return (
-                        <ListStyles key={id} component="li" className={props.classList.listItem}>
-                         
-                            <Typography key={id}>
-                                <div 
-                                style={{ cursor: "pointer" }}
-                                // onClick={() => history.push(`home/classes/${id}`)}
-                                >
-                                    <Typography variant="subtitle1" style={{ display:"flex", justifyContent:"space-between" }}>
-                                        {'hello'}
-                                    <DeleteModal 
-                                        deleteType="classes"
-                                        classId={id}
-                                        className={`material-icons ${props.classList.icons}`}
-                                        // onClick={e => removeClass(id)}
-                                        style={{ zIndex: "1000" }}
-                                        // removeClass={removeClass}
-                                    />
-                                    </Typography>
-                                    <div onClick={() => history.push(`home/classes/${id}`)}>
-                                        
-                                        <hr />
-                                        <Typography variant="overline">
-                                            {`Subject: ${subject}`}
-                                        </Typography>
-                                        <Typography variant="overline">
-                                            {`Grade Level: ${grade_level}`}
-                                        </Typography>
-                                        <Typography variant="overline">
-                                            {`Teacher Name: ${teacher_name}`}
-                                        </Typography>
-                                        <Typography variant="overline">
-                                            {`Number Of Students: ${number_of_students}`}
-                                        </Typography>
-                                    </div>
+  useEffect(() => {
+    getVolunteers();
+  }, [getVolunteers]);
 
-                                </div>
-                            </Typography>
-                        </ListStyles>
-                    )
-                 } 
-            )}
-        </div>
-    )
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      {getFiltered(volunteers).map(volunteer => {
+        const { id, name, approved, donator, email } = volunteer;
+
+        return (
+          <ListStyles key={id} component="li" style={{ width: "30%" }}>
+            <Typography style={{ width: "100%" }}>
+              <div style={{ cursor: "pointer" }}>
+                <Typography
+                  variant="subtitle1"
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  {name}
+                  <DeleteModal
+                    deleteType="volunteers"
+                    className={`material-icons ${classes.icons}`}
+                    volunteerId={id}
+                    style={{ zIndex: "1000" }}
+                  />
+                </Typography>
+                <div onClick={() => history.push(`home/classes/${id}`)}>
+                  <hr />
+                  <Typography variant="overline">{`Email: `}</Typography>
+                  <Typography variant="overline">{`${email}`}</Typography>
+                  <Typography variant="overline">
+                    {`Approved: ${approved}`}
+                  </Typography>
+                  <Typography variant="overline">
+                    {`Donator: ${donator}`}
+                  </Typography>
+                </div>
+              </div>
+            </Typography>
+          </ListStyles>
+        );
+      })}
+    </div>
+  );
 }
 
 const mapStateToProps = state => {
-    console.log('volunteer state', state)
-    return {
-      classList: state.classesReducer.classList
-    }
+  return {
+    volunteers: state.volunteerReducer.volunteers
+  };
 };
 
 export default connect(
-    mapStateToProps,
-    { getClassList, deleteClass }
-)(Overview);
+  mapStateToProps,
+  { getVolunteers }
+)(withStyles()(Overview));
