e75a8606ab6208203615af32a787e7f6f8f533de
Brandon <brandon@pop-os.localdomain>
2019-06-20 10:12:42 -0500

    New filter added

--- src/components/UI/SearchCard/filter.js
+++ src/components/UI/SearchCard/filter.js
@@ -1,47 +1,32 @@
-// ! FUNCTION MUST BE CLEANED UP AND DRY EVENTUALLY
-
 export default ({ items, pagination, volunteerFilter, search, ...rest }) => {
   const { offset, limit, setMax } = pagination;
   let filteredResults = [];
+  // Conditional statement to show approved or unapproved
+  const conditional =
+    volunteerFilter === "approved"
+      ? true
+      : volunteerFilter === "unapproved"
+      ? false
+      : null;
 
   if (volunteerFilter === "filter") {
     filteredResults = items;
-  }
-
-  if (volunteerFilter === "approved" || volunteerFilter === "unapproved") {
-    if (volunteerFilter === "unapproved") {
-      const unapproved = items.filter(item => {
-        for (let property in item) {
-          if (property === "approved") {
-            if (item[property] === false) {
-              return true;
-            }
+  } else {
+    filteredResults = items.filter(item => {
+      for (let property in item) {
+        if (property === "approved") {
+          if (item[property] === conditional) {
+            return true;
           }
         }
-        return false;
-      });
-      filteredResults = unapproved;
-    } else {
-      const approved = items.filter(item => {
-        for (let property in item) {
-          if (property === "approved") {
-            if (item[property] === true) {
-              return true;
-            }
-          }
-        }
-        return false;
-      });
-      filteredResults = approved;
-    }
+      }
+      return false;
+    });
   }
-  
+  // Filter by search term
   filteredResults = filteredResults.filter(item => {
     for (let property in item) {
-      const noSearch = [
-        "user_id",
-        "id"
-      ];
+      const noSearch = ["user_id", "id"];
       const string = item[property]
         ? item[property].toString().toLowerCase()
         : "";
